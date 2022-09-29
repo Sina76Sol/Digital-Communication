3f9a939ba892adef79b21be3ba0c95f047fb9caa
Sina76Sol <77147608+Sina76Sol@users.noreply.github.com>
2022-09-29 18:27:45 -0400

    Add files via upload

---
+++ First_project.m
@@ -0,0 +1,90 @@
+% First Computer project
+% Student name: Reza SoleimaniMajd
+% Student ID: 100880218
+clear;
+clc;
+%% Task one
+
+% Generate a set of 1000 i.i.d. random numbers
+X = 4 * rand(1000, 1) - 2;
+
+% Display histogram of the data:
+figure(1);
+h = histogram(X);
+
+% Get the value of each bin:
+disp("Bin values are:");
+disp(h.Values);
+
+% Calculate the pdf:
+pdf = h.Values/1000;
+
+% Calculate the CDF:
+CDF = cumsum(pdf);
+
+% Plot CDF:
+figure(2)
+plot(CDF)
+
+% Another way of plotting:
+sorted = sort(X);
+plot(sorted)
+
+%% Task two
+
+% Generate a set of 1000 i.i.d random Gausian numbers
+G_x = randn(1000, 1);
+
+% Display the histogram of data:
+figure(3);
+G_h = histogram(G_x);
+
+% Get the value of each bin:
+disp("Bin values are:");
+disp(G_h.Values);
+
+% Calculate the pdf:
+G_pdf = G_h.Values/1000;
+
+% Calculate the CDF:
+G_CDF = cumsum(G_pdf);
+
+% Plot CDF:
+figure(4);
+plot(G_CDF)
+
+%% Task three
+
+% Calculate autocorrolation funtion
+acf = autocorr(X, NumLags=50);
+figure(5)
+stem(acf, "filled");
+
+% Calculate power density spectrum
+S_x = fft(acf);
+figure(6);
+fshift = -25:25;
+stem(fshift, abs(fftshift(S_x)), "filled");
+
+%% Task four
+
+% Calculate the output of the linear filter:
+Y = zeros(size(X));
+Y_n_1 = 0;
+for i=1:length(X)
+    Y(i) = 0.95 * Y_n_1  + X(i);
+    Y_n_1 = Y(i);
+end
+
+% Calculate autocorrolation funtion of filtered random process
+acf_filtered = autocorr(Y, NumLags=50);
+figure(7)
+stem(acf_filtered, "filled");
+
+% Calculate power density spectrum of filtered random process
+S_y = fft(acf_filtered);
+figure(8);
+fshift = -25:25;
+stem(fshift, abs(fftshift(S_y)), "filled");
+
+
