b5da5102b88dd30d719a9847c9bda1f5f74aba1e
Sina76Sol <77147608+Sina76Sol@users.noreply.github.com>
2022-10-31 13:36:16 -0400

    Update QPSK.m

--- Second project/QPSK.m
+++ Second project/QPSK.m
@@ -1,37 +1,52 @@
 clear;
 clc;
-%% Building the constellation
-c = [sqrt(2)/2+sqrt(2)/2i, -sqrt(2)/2+sqrt(2)/2i,...
-    -sqrt(2)/2-sqrt(2)/2i, sqrt(2)/2-sqrt(2)/2i];
-sigpower = pow2db(mean(abs(c).^2));
-M = length(c);
+%% Building the constellation for QPSK
+c_QPSK = [sqrt(2)/2+sqrt(2)/2i, -sqrt(2)/2+sqrt(2)/2i,...
+         -sqrt(2)/2-sqrt(2)/2i, sqrt(2)/2-sqrt(2)/2i];
+QPSK_sigpower = pow2db(mean(abs(c_QPSK).^2));
+M = length(c_QPSK);
 
-data = randi([0 M-1],10^4,1);
-modData = genqammod(data,c);
+QPSK_x = randi([0 M-1],10^4,1);
+QPSK_mod_x = genqammod(QPSK_x,c_QPSK);
 
-rxSig = awgn(modData,3,sigpower);
+QPSK_y = awgn(QPSK_mod_x,3,QPSK_sigpower);
 
-% h = scatterplot(rxSig);
-% hold on
-% scatterplot(c,[],[],'r*',h)
-% grid
+h = scatterplot(QPSK_y);
+hold on
+scatterplot(c_QPSK,[],[],'r*',h)
+grid
 
 %% Experimental error rate
 SNR = 0:3:15;
 error_rate = zeros(size(SNR));
 for i = 1:length(SNR)
-    noisy_sig = awgn(modData,SNR(i),sigpower);
-    detected = genqamdemod(noisy_sig,c);
-    [numErrors,ser] = symerr(data,detected);
+    noisy_sig = awgn(QPSK_mod_x,SNR(i),QPSK_sigpower);
+    QPSK_detected = genqamdemod(noisy_sig,c_QPSK);
+    [numErrors,ser] = symerr(QPSK_x,QPSK_detected);
     error_rate(i) = ser;
 end
 
-plot(error_rate,'-o')
+semilogy(error_rate,'-o')
 hold on
-%% Imperical error rate
+%% Theoritical error rate
 delta =sqrt(2);
 Bit_energy = 0.5;
 sigma = sqrt(Bit_energy./(10.^(SNR./10)));
 Pe = 1-(1-qfunc(delta./(2*sigma))).^2;
-plot(Pe, 'r-*')
-legend("experimental", "imperical")
+semilogy(Pe, 'r-*')
+legend("experimental", "theoritical")
+
+%% Build the constellation for 4PAM
+delta  = sqrt(4/5);
+c_4pam = [-3*delta/2, -delta/2, delta/2, 3*delta/2];
+sigpower = pow2db(mean(abs(c_4pam).^2));
+M = length(c_4pam);
+
+QPSK_x = randi([0 M-1],10^4,1);
+modData = genqammod(data,c_4pam);
+
+rxSig = awgn(modData,3,sigpower);
+h = scatterplot(rxSig);
+hold on
+scatterplot(c_4pam,[],[],'r*',h)
+grid
